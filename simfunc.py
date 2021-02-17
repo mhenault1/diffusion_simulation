3c6ebd6ff70f97008908fc7e4fb722de0d4ed372
mhenault1 <mathieu.henault.1@ulaval.ca>
2021-02-17 13:07:34 -0500

    updated notebook and module functions for plotting and table display, 3nd try

--- simfunc.py
+++ simfunc.py
@@ -29,62 +29,60 @@ def plot_table(sim):
 
 	columns = [f't={t}' for t in range(duree)]
 	index = [f'x_{i+1}' for i in range(n_particules)]
+	df = DataFrame(sim, index=index, columns=columns).T.iloc[display_times]
+	df['x_barre'] = df[index].mean(axis=1)
+	df['x2_barre'] = df[index].var(axis=1)
+	return df
 
-	return DataFrame(sim, index=index, columns=columns).T.iloc[display_times]
+def plot_sim(sim):
 
-def plot_sim(sim, variable):
+	n_particules, duree = sim.shape
+	T = np.arange(sim.shape[1])
+	nbins = np.int32(np.floor(duree/2))
+	if nbins > 25:
+		nbins = 25
+	tp = np.int32(np.linspace(0, duree, nbins))
 
-	if variable not in [1,2]:
-		print('ERREUR: la variable y doit être égale a 1 ou 2.')
+	means = np.apply_along_axis(lambda x: np.mean(x), 0, sim)
+	vars = np.apply_along_axis(lambda x: np.mean(x**2), 0, sim)
 
-	else:
-		n_particules, duree = sim.shape
-		T = np.arange(sim.shape[1])
+	fig, (ax0, ax1, ax2) = plt.subplots(figsize=[15,5], ncols=3,
+	gridspec_kw={'wspace':0.3, 'left':0.07, 'right':0.95})
+	fig.patch.set_facecolor('white')
+	fig.text(0.5, 0.97, f'{n_particules} particules, durée: {duree-1}', ha='center', va='top', size=30)
 
-		nbins = np.int32(np.floor(duree/2))
-		if nbins > 25:
-		    nbins = 25
-		tp = np.int32(np.linspace(0, duree, nbins))
+	cam = Camera(fig)
 
-		fig, (ax0, ax1) = plt.subplots(figsize=[13,6], ncols=2, gridspec_kw={'wspace':0.3})
-		fig.patch.set_facecolor('white')
-		fig.text(0.5, 0.97, f'{n_particules} particules, durée: {duree-1}', ha='center', va='top', size=30)
-		cam = Camera(fig)
+	ylim = 1.1*np.abs(sim.max()) * np.array([-1,1])
+	ax0.set_facecolor('0.95')
+	ax0.set_xlabel(r'Temps $t$ (secondes)', size=20)
+	ax0.set_ylabel(r'Position $x$', size=20)
+	ax0.set_ylim(ylim)
 
-		ylim = 1.1*np.abs(sim.max()) * np.array([-1,1])
-		ax0.set_facecolor('0.95')
-		ax0.set_xlabel(r'Temps $t$ (secondes)', size=20)
-		ax0.set_ylabel(r'Position $x$', size=20)
-		ax0.set_ylim(ylim)
+	ax1.set_facecolor('0.95')
+	ax1.set_xlabel(r'Temps $t$ (secondes)', size=20)
+	ax1.set_ylabel(r'Moyenne $\overline{x}$', size=20)
+	ax1.set_ylim(ylim)
 
-		ax1.set_facecolor('0.95')
-		ax1.set_xlabel(r'Temps $t$ (secondes)', size=20)
+	ax2.set_facecolor('0.95')
+	ax2.set_xlabel(r'Temps $t$ (secondes)', size=20)
+	ax2.set_ylabel(r'Variance $\overline{x^2}$', size=20)
 
-		c = [cm.viridis(i) for i in np.linspace(0, 1, n_particules)]
+	c = [cm.viridis(i) for i in np.linspace(0, 1, n_particules)]
 
-		if variable == 1:
-		    ax1.set_ylabel(r'Moyenne $\overline{x}$', size=20)
-		    ax1.set_ylim(ylim)
-		    values = np.apply_along_axis(lambda x: np.mean(x), 0, sim)
-		    def add_baseline():
-		        ax1.axhline(0, lw=0.5, c='k')
+	for t in tp:
+		for i, x in enumerate(sim):
+		    ax0.plot(T[:t], x[:t], alpha=0.5, lw=1, c=c[i])
 
-		if variable == 2:
-		    ax1.set_ylabel(r'Variance $\overline{x^2}$', size=20)
-		    values = np.apply_along_axis(lambda x: np.mean(x**2), 0, sim)
-		    def add_baseline():
-		        ax1.plot([0,duree], [0, duree], lw=0.5, c='k')
+		ax1.plot(T[:t], means[:t], c='k')
+		ax1.axhline(0, lw=0.5, c='k')
 
-		for t in tp:
-		    for i, x in enumerate(sim):
-		        ax0.plot(T[:t], x[:t], alpha=0.5, lw=1, c=c[i])
+		ax2.plot(T[:t], vars[:t], c='k')
+		ax2.plot([0,duree], [0, duree], lw=0.5, c='k')
 
-		    add_baseline()
-		    ax1.plot(T[:t], values[:t], c='k')
+		cam.snap()
 
-		    cam.snap()
+	animation = cam.animate()
 
-		animation = cam.animate()
-
-		display(HTML(animation.to_jshtml()))
-		plt.close()
+	display(HTML(animation.to_jshtml()))
+	plt.close()
